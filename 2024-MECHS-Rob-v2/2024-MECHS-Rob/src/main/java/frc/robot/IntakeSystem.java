2d582147e75851233220172793d77f92612ed769
Gino1102 <jeremycode1125@gmail.com>
2024-03-05 19:04:11 -0600

    Moving over to 2024 softwar.e

---
+++ 2024-MECHS-Rob-v2/2024-MECHS-Rob/src/main/java/frc/robot/IntakeSystem.java
@@ -0,0 +1,24 @@
+package frc.robot;
+
+import com.ctre.phoenix.motorcontrol.can.VictorSPX;
+import com.ctre.phoenix.motorcontrol.VictorSPXControlMode;
+public class IntakeSystem {
+    private VictorSPX Motor_Intake;
+
+    public IntakeSystem() {
+        
+        this.Motor_Intake = new VictorSPX(RobotConstants.Motor_Channel_Intake);
+        this.Motor_Intake.setInverted(true);
+    }
+
+    public void update(boolean Intake_Status, boolean reverseIntakeButton) {
+        if (Intake_Status == true) {
+            this.Motor_Intake.set(VictorSPXControlMode.PercentOutput, 0.5);
+       } else if (reverseIntakeButton == true) {
+            this.Motor_Intake.set(VictorSPXControlMode.PercentOutput, -0.5);
+        } else {
+            this.Motor_Intake.set(VictorSPXControlMode.PercentOutput, 0.0);
+        }
+        // Posible logic issue here could better nest the inverted toggle value
+    }
+}
